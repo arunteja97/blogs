af3d997a4de950a8b881b918188a5aa170d370fa
arunteja97 <arun.teja1919@gmail.com>
2023-11-20 23:30:56 +0530

    backend

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import 'dotenv/config'
+import ServerlessHttp from 'serverless-http';
 
 import { createUser, signInUser, signOutUser } from './utils/firebase/auth.utils.js';
 import { getBlogs, addBlog, deleteBlog, updateBlog,
@@ -11,62 +12,67 @@ import { getBlogs, addBlog, deleteBlog, updateBlog,
 
 const port = process.env.PORT || 5000;
 const app = express();
+const router = express.Router();
 
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.get('/categories', async (req, res) => {
+router.get('/categories', async (req, res) => {
     const categories = await getCategories();
     res.status(200).send(categories)
 })
 
-app.post('/addcategory', async (req, res) => {
+router.post('/addcategory', async (req, res) => {
     const result = await addCategory(req.body.name)
     res.send(result)
 })
 
-app.get('/blogs', async (req, res) => {
+router.get('/blogs', async (req, res) => {
     const blogs = await getBlogs();
     res.send(blogs);
 });
 
-app.post('/addblog', async (req, res) => {
+router.post('/addblog', async (req, res) => {
     const response = await addBlog(req.body)
     res.status(200).send(response);
 });
 
-app.delete('/deleteblog', async(req, res) => {
+router.delete('/deleteblog', async(req, res) => {
     const response = await deleteBlog(req.body.blogId)
     res.status(200).send(response);
 })
-app.put('/updateblog', async(req, res) => {
+router.put('/updateblog', async(req, res) => {
     const response = await updateBlog(req.body)
     res.status(200).send(response);
 })
 
-app.get('/blog/:id', async(req, res) => {
+router.get('/blog/:id', async(req, res) => {
     const blog = await getBlog(req.params.id)
     res.send(blog)
 })
 
-app.post('/signup', async (req, res) => {
+router.post('/signup', async (req, res) => {
     const user = await createUser(req.body.email, req.body.password);
     res.send('user created successfully')
 })
 
-app.get('/signout', async (req, res) => {
+router.get('/signout', async (req, res) => {
     const user = await signOutUser();
     res.send('user signedout')
 })
 
-app.post('/signin', async (req, res) => {
+router.post('/signin', async (req, res) => {
     const user = await signInUser(req.body.email, req.body.password);
     res.send(`user signedin`)
 })
 
-app.listen(port, async () => {
-    console.log(`Listening on port ${port}`)
-});
+app.use('/.netlify/functions/api', router)
+
+// app.listen(port, async () => {
+//     console.log(`Listening on port ${port}`)
+// });
+
+export const handler = ServerlessHttp(app)
 
